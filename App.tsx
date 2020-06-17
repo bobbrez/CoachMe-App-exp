95ccc7778aff376733e921085f8ece212b7ad404
Bob Breznak <bob.breznak@gmail.com>
2020-06-17 13:55:58 -0400

    Add basic navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './Components/HomeScreen';
+import { DetailsScreen } from './Components/DetailsScreen'
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { default as theme } from './theme.json';
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text category='h1'>HOME</Text>
-  </Layout>
+const { Navigator, Screen } = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Navigator headerMode='none'>
+    <Screen name='Home' component={HomeScreen}/>
+    <Screen name='Details' component={DetailsScreen}/>
+  </Navigator>
+);
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <HomeNavigator/>
+  </NavigationContainer>
 );
 
 export default () => (
-  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-    <HomeScreen />
-  </ApplicationProvider>
+  <>
+    <IconRegistry icons={EvaIconsPack}/>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <AppNavigator />
+    </ApplicationProvider>
+  </>
 );
